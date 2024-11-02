f8ec86267f75444ae5c490916b7c9115b8922dba
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-02 21:26:10 +0100

    setting up jest configurations

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  preset: "ts-jest",
+  clearMocks: true,
+  forceExit: true,
+  collectCoverage: true,
+  verbose: true,
+  forceExit: true,
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  collectCoverageFrom: ["<rootDir>/src/api/**/*.ts"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+  testMatch: ["**/**/*.test.ts", "**/**/*.spec.ts"],
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+};
