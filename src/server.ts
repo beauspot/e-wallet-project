66a9d109584bfaa2b8e4784ad278b856d8f3cd14
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-03 16:45:07 +0100

    created a CreateServer Class & setup a serverInit method on the class instantiating it to start the server & also set up the dotenv module to load env variables.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,36 @@
-import { App } from "@/app";
+import dotenv from "dotenv";
+import ip from "ip";
 
-(() => {
-  const app = new App();
-  try {
-    app.listen();
-  } catch (error: any) {
-    logging.error(error.message);
+import "@/utils/logging";
+import { CreateAppServer } from "@/app";
+import { db_init } from "@/configs/db.config";
+
+dotenv.config();
+
+const Port: number = Number(process.env.SERVER_PORT) || 3000;
+const appServer = new CreateAppServer(Port);
+const applicationMessage = "Application is running on:";
+
+class CreateServer {
+  constructor(private server: CreateAppServer,
+    private port: typeof Port,
+    private applicationMessage: string) { }
+  
+  async serverInit():Promise<void> {
+    try {
+      await db_init();
+      this.server.listen(this.port);
+
+      logging.log("----------------------------------------");
+      logging.log(`Documentation with swagger`);
+      logging.log("----------------------------------------");
+      logging.log(`${this.applicationMessage} ${ip.address()}:${this.port}`);
+      logging.log("----------------------------------------");
+    } catch (error) {
+      logging.error("Database connection error: " + error);
+    }
   }
-})();
+};
+
+const server = new CreateServer(appServer, Port, applicationMessage);
+server.serverInit();
