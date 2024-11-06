a12f36307fc88680ba43505ed5640046a9881118
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-06 11:23:27 +0100

    finished setting up the wallet database

---
+++ src/api/db/wallet.entity.ts
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from "uuid";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BeforeInsert,
+  CreateDateColumn,
+  OneToOne,
+  JoinColumn,
+  BaseEntity
+} from "typeorm";
+
+import { User } from "@/db/user.entity"
+
+@Entity()
+export class UserWallet extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ default: 0.0 })
+  balance: number
+
+  // TODO: the transaction pin must be hashed
+  @Column()
+  transaction_pin: number;
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @OneToOne(() => User, (user) => user.wallet)
+  @JoinColumn()
+  user: User;
+
+  @BeforeInsert()
+  generateId() {
+    this.id = `walletID-${uuidv4()}`;
+  }
+};
