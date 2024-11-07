7e1c204b5b298d1b62b3a01efcbf36753b243d00
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-07 10:39:45 +0100

    finished setting up the settlement accounts model.

---
+++ src/api/db/settlementAccts.entity.ts
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from "uuid";
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    BeforeInsert,
+    JoinColumn,
+    OneToOne
+} from "typeorm";
+
+import { User } from "@/db/user.entity";
+
+@Entity()
+export class SettlementAcct {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ nullable: false })
+    accountNumber: number;
+
+    @Column({ nullable: false })
+    accountName: string;
+
+    @Column({ nullable: false, default: false })
+    default: boolean;
+
+    @OneToOne(() => User, (user) => user.settlementAcct)
+    @JoinColumn()
+    userAcct: User;
+
+    @BeforeInsert()
+    generateId() {
+        this.id = `SettleAcctID-${uuidv4()}`;
+    }
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
