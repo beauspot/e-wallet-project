2c1b34468c2f27127585b87c260704ca56e0726c
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-08 23:37:41 +0100

    finished setting up the interfaces for the various payloads

--- src/api/helpers/interfaces/flutterwave.interface.ts
+++ src/api/helpers/interfaces/flutterwave.interface.ts
@@ -10,13 +10,40 @@ interface CardChargePayload {
     phone_number?: string;
     fullname?: string;
     pin?: string;
-}
+};
 
 interface AuthorizeCardPaymentPayload {
     otp: string;
     flw_ref: string;
     userId: string;
+};
+
+interface TransferPayload {
+    account_no: string;
+    amount: number;
+    recipient: string;
+    bank?: string;
+    currency: string;
+    receipientBankCode?: string;
+    reference?: string;
+    narration?: string;
+    senderId?: string;
+    transferType: "Wallet" | "3rd-Party" 
+    callback_url?: string;
+};
+
+interface SubAccounts {
+    id?: string;  
+    account_bank: string;
+    account_number: string;
+    business_name: string;  
+    business_email: string;
+    business_contact: string;  
+    business_contact_mobile: string; 
+    business_mobile?: string;  
+    split_type?: string;  
+    split_value?: number;
 }
 
 
-export { CardChargePayload, AuthorizeCardPaymentPayload };
+export { CardChargePayload, AuthorizeCardPaymentPayload, TransferPayload, SubAccounts };
