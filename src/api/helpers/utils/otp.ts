b90801ecbfae44c68f4fb043dcc124c8935e36be
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-08 21:14:50 +0100

    otp setup with twilio

---
+++ src/api/helpers/utils/otp.ts
@@ -0,0 +1,46 @@
+import { User } from "@/db/user.entity";
+import AppError from "@/utils/appErrors";
+import { AppDataSource } from "@/configs/db.config";
+
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSid = process.env.TWILIO_SERVICE_SID;
+
+const sendOtp = async (phoneNumber: string) => {
+    const existingUser = await AppDataSource.getRepository(User).findOne({
+        where: { phoneNumber }
+    });
+
+    if (existingUser) {
+        throw new AppError(`user already exists, Login`, "400", false);
+    }
+    const client = require("twilio")(accountSid, authToken);
+
+    try {
+        const verification = await client.verify.v2
+            .services(serviceSid)
+            .verifications.create({ to: phoneNumber, channel: "sms" });
+        return verification;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+const verifyOtp = async (phoneNumber: string, otp: number) => {
+    const client = require("twilio")(accountSid, authToken);
+
+    try {
+        const verificationCheck = await client.verify.v2
+            .services(serviceSid)
+            .verificationChecks.create({ to: phoneNumber, code: otp });
+        console.log("verificationCheck", verificationCheck);
+        return verificationCheck.status;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export { sendOtp, verifyOtp };
