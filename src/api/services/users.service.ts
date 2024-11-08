b990cbdfd96b2bfb356df55880b44652e6e492b9
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-08 21:15:19 +0100

    setting up the user service

--- src/api/services/users.service.ts
+++ src/api/services/users.service.ts
@@ -1,2 +1,9 @@
-import { dataSource } from "@/configs/db.config";
+import { promisify } from "util";
+import * as jwt from "jsonwebtoken";
+
+
+
 import { User } from "@/db/user.entity";
+import {sendOtp, verifyOtp} from "@/utils/otp"
+import { UserWallet } from "@/db/wallet.entity";
+import { AppDataSource } from "@/configs/db.config";
