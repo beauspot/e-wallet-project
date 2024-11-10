46528c9ee1dabf481290372f39e5dd65842d13de
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-10 16:28:23 +0100

    setup the interface for the user

--- src/api/helpers/interfaces/user.interface.ts
+++ src/api/helpers/interfaces/user.interface.ts
@@ -1,11 +1,17 @@
 import { gender_enum, userRole } from "@/enum/user.enum";
 
 export interface userInterface {
-  fullName: string;
-  gender: gender_enum;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  phoneNumber: string;
   email: string;
+  dob: Date;
   password: string;
+  nin: string;
   bvn: string;
+  gender: gender_enum;
   role: userRole;
+  account_no: string;
   date_of_birth: Date;
 }
