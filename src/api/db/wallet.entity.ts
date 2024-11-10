0aeddfcb5196031c334d7c09821e72b233d8f62a
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-10 16:28:50 +0100

    changed transaction_pin on the UserWallet entity from number to string.

--- src/api/db/wallet.entity.ts
+++ src/api/db/wallet.entity.ts
@@ -22,7 +22,7 @@ export class UserWallet extends BaseEntity {
 
   // TODO: the transaction pin must be hashed
   @Column()
-  transaction_pin: number;
+  transaction_pin: string;
 
   @OneToOne(() => User, (user) => user.wallet)
   @JoinColumn()
