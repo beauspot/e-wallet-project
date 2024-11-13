f5ca2f16668993d95ddff174a8a1afbee2da4099
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-13 12:53:51 +0100

    implemented the zod middleware for validating inputs

--- src/api/helpers/middlewares/validate.ts
+++ src/api/helpers/middlewares/validate.ts
@@ -1,15 +1,30 @@
-import { AnyZodObject } from "zod";
-import expressAsyncHandler from "express-async-handler";
+import { AnyZodObject, ZodError } from "zod";
 import { Request, Response, NextFunction } from "express";
 
-export const validateResource = (schema: AnyZodObject) =>
-  expressAsyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      schema.parse({
-        body: req.body,
-        query: req.query,
-        params: req.params,
-      });
-      next();
-    }
-  );
+export const validate =
+  (schema: AnyZodObject) =>
+    (req: Request, res: Response, next: NextFunction) => {
+      try {
+        schema.parse({
+          body: req.body,
+          query: req.query,
+          params: req.params,
+        });
+        next();
+      } catch (e: any) {
+        if (e instanceof ZodError) {
+          const formattedErrors = e.errors.map((err) => ({
+            message: err.message,
+            path: err.path.join(" > "),  // Joins the path array for readability
+          }));
+
+          // Send the formatted errors in the response
+          return res.status(400).json({
+            error_message: formattedErrors,
+          });
+        }
+
+        // If the error is not a ZodError, forward it
+        next(e);
+      }
+    };
