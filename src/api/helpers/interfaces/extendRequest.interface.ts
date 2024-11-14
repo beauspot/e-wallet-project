0a7458c594d6314869926ca73f7196b4d184b4d9
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-14 14:52:02 +0100

    made the user object to be undefined or a available same as the id which could be a string or undefined.

--- src/api/helpers/interfaces/extendRequest.interface.ts
+++ src/api/helpers/interfaces/extendRequest.interface.ts
@@ -1,7 +1,9 @@
 import { Request } from "express";
 
+// TODO: would come back to this issue
+import { User } from "@/db/user.entity";
 export interface ExtendRequest extends Request {
-    user: {
-        id: string
+    user?: {
+        id?: string
     }
 }
