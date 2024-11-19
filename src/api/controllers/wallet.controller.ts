a013efc6b0cc2f5386c1dfc16ff0e57e31f8543b
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-19 16:09:50 +0100

    added the deposit, authorize, & transfer endpoint

--- src/api/controllers/wallet.controller.ts
+++ src/api/controllers/wallet.controller.ts
@@ -38,4 +38,23 @@ export class WalletController {
             data: updatedWallet,
         });
     });
+
+    deposit = catchAsync(async (req: ExtendRequest, res: Response, next: NextFunction) => {
+        if (!req.user || !req.user.email)
+            throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
+        const response = await this.walletService.deposit(req.body, req.user.email);
+        res.status(200).json({ success: true, data: response });
+    });
+
+    authorize = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const response = await this.walletService.authorize(req.body, req.session);
+        res.status(200).json({ success: true, message: "Charge on card initiated", data: response });
+    });
+
+    transfer = catchAsync(async (req: ExtendRequest, res: Response, next: NextFunction) => {
+        if (!req.user || !req.user.id)
+            throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
+        const response = await this.walletService.transfer(req.body, req.user.id);
+        res.status(200).json({ success: true, message: "Transfer initiated", data: response });
+    });
 }
