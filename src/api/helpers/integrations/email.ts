39b949743b6a07ad0945fd526e4228f324afd4e9
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-19 16:10:49 +0100

    moved this file tpo the integrations directory

---
+++ src/api/helpers/integrations/email.ts
@@ -0,0 +1,52 @@
+import * as nodemailer from "nodemailer";
+
+interface User {
+    email: string;
+    firstName: string;
+}
+
+export class Email {
+    private to: string;
+    private firstName: string;
+    private resetToken: string;
+    private from: string;
+
+    constructor(user: User, resetToken: string) {
+        this.to = user.email;
+        this.firstName = user.firstName;
+        this.resetToken = resetToken;
+        this.from = ` E-walletPay <${process.env.EMAIL_FROM}>`;
+    }
+
+    private newTransport() {
+        return nodemailer.createTransport({
+            service: process.env.EMAIL_SERVICE || "Gmail",
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+    }
+
+    // Send the actual email
+    private async send(subject: string, template: string) {
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html: template,
+        };
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send("Welcome", `<h1>Welcome to E-walletPay, ${this.firstName}!</h1>`);
+    }
+
+    async sendPasswordReset() {
+        await this.send(
+            "Password Reset OTP",
+            `<p>Forgot your password? Your One-Time Password to reset your password is <strong>${this.resetToken}</strong>. It is valid for only 10 minutes.</p>`
+        );
+    }
+};
