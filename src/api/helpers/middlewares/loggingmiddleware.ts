0cb35f521f95617818762e8dd0b54645173f044d
beauspot <42305168+beauspot@users.noreply.github.com>
2024-11-19 06:22:56 +0100

    fixed the logging error on the login middleare  module.

--- src/api/helpers/middlewares/loggingmiddleware.ts
+++ src/api/helpers/middlewares/loggingmiddleware.ts
@@ -1,16 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 
+import logging from "@/utils/logging";
+
 export function logging_middleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  logging.log(
+  logging.info(
     `Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
   );
 
   res.on("finish", () => {
-    logging.log(
+    logging.info(
       `Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`
     );
   });
