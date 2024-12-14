f3cbe74c492df2747faf29675f70fa05a588e92e
beauspot <42305168+beauspot@users.noreply.github.com>
2024-12-14 19:46:21 +0100

    deleted this file

--- src/api/types/morx.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "morx" {
-    interface Spec {
-        build(field: string, rule: string): Spec;
-        end(): Spec;
-    }
-    export function spec(): Spec;
-    export function validate(data: any, spec: Spec, options?: { throw_error: boolean }): { params: any };
-}
